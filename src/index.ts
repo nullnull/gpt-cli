84b02cf6a0f3346ec973cbf2aa329a409d17789f
Katsuma Narisawa <katsuma.narisawa@gmail.com>
2023-08-12 16:54:12 +0900

    Setup commander and zod

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,35 @@
 import { foo } from './foo'
+import { program } from 'commander'
+import z from 'zod'
 
-console.log('hello world')
-foo()
+const argsSchema = z.object({
+  prompt: z.string().optional(),
+  first: z.boolean().optional(),
+  separator: z.string().optional(),
+})
+type GptCliArgs = z.infer<typeof argsSchema>
+
+function main() {
+  program
+    .option('--first')
+    .option('-s, --separator <char>')
+    .argument('<prompt>', 'prompt')
+    .action((prompt, options) => {
+      console.log('hello world', options.first, prompt)
+      const args = argsSchema.parse({
+        ...options,
+        prompt,
+      })
+      console.log(args)
+      execute(args)
+    })
+
+  program.parse()
+}
+
+function execute({ prompt }: GptCliArgs) {
+  console.log('prompt:', prompt)
+  foo()
+}
+
+main()
