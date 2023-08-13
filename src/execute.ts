f8b55a791e5aca5d0e6397b415b1f94a73c46c64
Katsuma Narisawa <katsuma.narisawa@gmail.com>
2023-08-13 09:27:45 +0900

    Convert command option to command

--- src/execute.ts
+++ src/execute.ts
@@ -5,7 +5,7 @@ import { executeReplaceFileTask } from './tasks/executeReplaceFileTask'
 import { openFile } from './util'
 import { executeCommandTask } from './tasks/executeCommandTask'
 
-export async function execute({ prompt, stdin, ...options }: GptCliArgs) {
+export async function execute({ prompt, ...options }: GptCliArgs) {
   // TODO: help, version
 
   const apiKey = process.env.OPENAI_API_KEY // TODO: move to configuration file
@@ -19,6 +19,12 @@ export async function execute({ prompt, stdin, ...options }: GptCliArgs) {
     process.exit(1)
   }
 
+  // コマンドの実行
+  if (options.type === 'command') {
+    await executeCommandTask({ apiKey, prompt, force: options.force, minimum: options.minimum })
+    process.exit(0)
+  }
+
   const filePath = options.file
   const fileBody = filePath !== undefined ? await openFile(filePath) : undefined
 
@@ -26,7 +32,7 @@ export async function execute({ prompt, stdin, ...options }: GptCliArgs) {
   // タスクごとに分岐
   //
   // ファイルの書き換えタスク
-  if (options.replace) {
+  if (options.write) {
     if (filePath === undefined || fileBody === undefined) {
       console.log(chalk.red('file is required'))
       process.exit(1)
@@ -35,15 +41,9 @@ export async function execute({ prompt, stdin, ...options }: GptCliArgs) {
     process.exit(0)
   }
 
-  // コマンドの実行
-  if (options.command) {
-    await executeCommandTask({ apiKey, prompt, force: options.force, minimum: options.minimum })
-    process.exit(0)
-  }
-
   // 単純にプロンプトを返す
   // ファイル、標準入力があればそれも指定する
-  const reply = await executeChatTask({ apiKey, prompt, extraContent: fileBody ?? stdin })
+  const reply = await executeChatTask({ apiKey, prompt, extraContent: fileBody ?? options.stdin })
   console.log(reply)
   process.exit(0)
 }
--- src/index.ts
+++ src/index.ts
@@ -3,31 +3,53 @@ import z from 'zod'
 import { execute } from './execute'
 import { logger } from './logger'
 
-const argsSchema = z.object({
+const chatCommandArgs = z.object({
+  type: z.literal(`chat`),
   prompt: z.string().optional(),
   file: z.string().optional(),
   stdin: z.string().optional(),
-  replace: z.boolean().optional(),
+  write: z.boolean().optional(),
+})
+
+const commandCommandArgs = z.object({
+  type: z.literal(`command`),
+  prompt: z.string().optional(),
   minimum: z.boolean().optional(), // gptの返信を最低限のものにし、また表示されるログも最低限にする
   force: z.boolean().optional(),
-  command: z.boolean().optional(),
 })
-export type GptCliArgs = z.infer<typeof argsSchema>
+const commandsArgs = z.union([chatCommandArgs, commandCommandArgs])
+export type GptCliArgs = z.infer<typeof commandsArgs>
 
 function main({ stdin }: { stdin?: string }) {
+  // commandコマンド
   program
-    .option('-c, --force')
-    .option('--command')
+    .command(`command`)
+    .description(`execute command`)
     .option('-m, --minimum')
-    .option('-r, --replace')
-    .option('-f, --file <char>')
-    .argument('<prompt>', 'prompt')
+    .option('-f, --force')
     .action((prompt, options) => {
-      logger.debug('hello world', options.minimum, prompt)
-      const args = argsSchema.parse({
+      logger.debug(options, prompt)
+      const args = commandCommandArgs.parse({
         ...options,
         prompt,
         stdin,
+        type: 'command',
+      })
+      logger.debug(args)
+      execute(args)
+    })
+
+  program
+    .option('-w, --write')
+    .option('-f, --file <char>')
+    .argument('<prompt>', 'prompt')
+    .action((prompt, options) => {
+      logger.debug(options, prompt)
+      const args = chatCommandArgs.parse({
+        ...options,
+        prompt,
+        stdin,
+        type: 'chat',
       })
       logger.debug(args)
       execute(args)
