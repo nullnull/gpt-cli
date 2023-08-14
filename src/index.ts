e5e195f370473176cb6804fbb9ae3ff0d0409fe0
Katsuma Narisawa <katsuma.narisawa@gmail.com>
2023-08-14 21:30:47 +0900

    Update version

--- src/index.ts
+++ src/index.ts
@@ -28,7 +28,7 @@ const commandsArgs = z.union([chatCommandArgs, commandCommandArgs])
 export type GptCliArgs = z.infer<typeof commandsArgs>
 
 function main({ stdin }: { stdin?: string }) {
-  program.version('0.1.1')
+  program.version('0.1.2')
 
   // register api key
   program
